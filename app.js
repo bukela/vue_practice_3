a3232e75bc86e0b81d286bc08c7a9fedd130b6f2
bukela <bukela@gmail.com>
2018-02-22 13:51:11 +0100

    initial

---
+++ app.js
@@ -0,0 +1,25 @@
+var one = new Vue({
+  	el: '#app-one',
+  	data: {
+		title: 'vue app one',
+	},
+	methods: {
+
+	},
+	computed: {
+
+	}
+});
+
+var two = new Vue({
+	el: '#app-two',
+	data: {
+		title: 'vue app two',
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+});
