e00fc42217e933a9df7d4a5ba58cd83bca8a52eb
bukela <bukela@gmail.com>
2018-02-22 16:03:06 +0100

    refs

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+Vue.component('greeting',{
+	template: '<p class="reusable">{{ name }} says helloo from reusable component <button v-on:click="changeName">Change name</button></p>',
+	data: function() {
+		return {
+			name: 'Loodie'
+		}
+	},
+	methods: {
+		changeName: function() {
+			this.name = 'Moodie changed';
+		}
+	}
+});
 var one = new Vue({
   	el: '#app-one',
   	data: {
 		title: 'vue app one',
 	},
 	methods: {
-
+		readRefs: function() {
+			console.log(this.$refs);
+		}
 	},
 	computed: {
-
+		greet: function() {
+			return 'hello from app one';
+		}
 	}
 });
 
@@ -17,9 +34,15 @@ var two = new Vue({
 		title: 'vue app two',
   },
   methods: {
-
+	changeTitle: function() {
+		one.title = 'Title changed';
+	}
   },
   computed: {
-
+	greet: function() {
+		return 'hello from app two';
+	}
   }
 });
+
+// two.title = 'outside change';  //radi i van vue intance
